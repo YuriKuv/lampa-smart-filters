d2d80388aea32bf3e92dc414727a92f4c5b6c48c
YuriKuv <yuri3kuvshinov@gmail.com>
2026-04-03 19:03:04 +0300

    Update yb.js

--- yb.js
+++ yb.js
@@ -213,7 +213,7 @@
                         <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="currentColor"/>
                     </svg>
                 </div>
-                <div class="menu__text">🗑 Удалить все закладки</div>
+                <div class="menu__text">Удалить все закладки</div>
             </li>
         `);
         
@@ -421,7 +421,6 @@
         applyButtonPositions();
         updateFiltersMenu();
         addSettings();
-        showMsg('✓ Плагин загружен. Настройки в разделе "Интерфейс"');
     }
     
     if (typeof Lampa !== 'undefined') {
